28f2f2043a1e957d91990e4aea9c79d033b91535
cornel <ghita.cornel@gmail.com>
2025-02-18 00:08:47 +0200

    initial

--- src/main/java/mqKafka/kafka/consumer/KafkaConsumer.java
+++ src/main/java/mqKafka/kafka/consumer/KafkaConsumer.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class MDMConsumer {
+public class KafkaConsumer {
 
     @KafkaListener(topics = "mdmTopicName")
     public void consumeMessage(MessageDataModel message) {
--- src/main/java/mqKafka/kafka/producer/KafkaProducer.java
+++ src/main/java/mqKafka/kafka/producer/KafkaProducer.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MDMProducer {
+public class KafkaProducer {
 
     @Value(value = "${kafka.mdmTopicName}")
     private String topicName;
