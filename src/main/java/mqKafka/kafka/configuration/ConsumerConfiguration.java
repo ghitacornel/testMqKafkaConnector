13948521ddc2bd793a4be692288223e7e2064b72
cornel <ghita.cornel@gmail.com>
2025-02-18 00:25:17 +0200

    initial

--- src/main/java/mqKafka/kafka/configuration/ConsumerConfiguration.java
+++ src/main/java/mqKafka/kafka/configuration/ConsumerConfiguration.java
@@ -21,23 +21,6 @@ class ConsumerConfiguration {
     private String bootstrapAddress;
 
     @Bean
-    ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        return factory;
-    }
-
-    @Bean
     ConsumerFactory<String, MessageDataModel> consumerDataModelFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
