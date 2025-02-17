047b7cc337e93ad8268945a81b983de0ed9658b3
cornel <ghita.cornel@gmail.com>
2025-02-18 00:18:05 +0200

    initial

--- src/main/java/mqKafka/kafka/producer/KafkaProducer.java
+++ src/main/java/mqKafka/kafka/producer/KafkaProducer.java
@@ -18,8 +18,8 @@ public class KafkaProducer {
     private final KafkaTemplate<String, MessageDataModel> kafkaTemplate;
 
     public void sendMessage(MessageDataModel message) {
-        log.info("Kafka produced message : {}", message);
         kafkaTemplate.send(topicName, message);
+        log.info("Kafka produced message : {}", message);
     }
 
 }
