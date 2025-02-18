9e7f3a605a399810d15f7ad1e2777d96b0f70bcd
cornel <ghita.cornel@gmail.com>
2025-02-18 09:56:24 +0200

    initial

--- src/main/java/mqKafka/translator/TranslatorConfig.java
+++ src/main/java/mqKafka/translator/TranslatorConfig.java
@@ -15,6 +15,15 @@ public class TranslatorConfig {
 
     private final KafkaProducer kafkaProducer;
 
+    // TODO
+    // TODO exactly 1 delivery
+    // need to check JMS transactions
+    // need to check KAFKA message acknowledgement
+
+    // TODO message ordering
+    // IBM MQ guarantees message ordering
+    // KAFKA guarantees message ordering per partition, currently we write in a single partition
+
     @JmsListener(destination = JMSQueueConfig.QUEUE_1)
     public void listenerForQueue1(MessageDataModel message) {
         kafkaProducer.sendMessage(message);
