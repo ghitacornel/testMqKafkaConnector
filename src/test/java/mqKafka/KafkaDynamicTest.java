37a359c6d3274be45922ec0878085205fa0c5fe9
cornel <ghita.cornel@gmail.com>
2025-02-25 10:37:58 +0200

    initial

--- src/test/java/mqKafka/KafkaDynamicTest.java
+++ src/test/java/mqKafka/KafkaDynamicTest.java
@@ -1,25 +1,29 @@
 package mqKafka;
 
+import mqKafka.dynamic.DynamicService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @EnableKafka
 @SpringBootTest
-@Import(KafkaTestConfiguration.class)
 @EmbeddedKafka(
         partitions = 1,
-        controlledShutdown = false,
+        controlledShutdown = true,
         brokerProperties = {
                 "listeners=PLAINTEXT://localhost:29092",
                 "port=29092"
         })
 public class KafkaDynamicTest {
 
+    @Autowired
+    DynamicService dynamicService;
+
     @Test
     public void testKafkaDynamic() {
-
+        dynamicService.unregister("queue1");
+        dynamicService.unregister("queue2");
     }
 }
